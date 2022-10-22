bc13d0e27ca0e4ae8637188b7e52a9d8a2b93bf4
gallyamovann <gallyamova.nelli@gmail.com>
2022-10-22 13:04:28 +0300

    updated

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from matplotlib import pyplot as plt
 from pathlib import Path
 
 PNG_FORMAT = "*.png"
-
+RGB_WHITE = (255, 255, 255)
+RGB_BLACK = (0, 0, 0)
+THICKNESS = 5
+CONTOUR_IDX = -1
+BORDER = 10
 
 def demonstration_items(path_items):
     paths = []
@@ -16,17 +20,13 @@ def demonstration_items(path_items):
         # загрузка изображения
         image = cv2.imread(str(p))
         img, binary, polys = get_fill_masks(image.copy())
-        RGB = (255, 255, 255)
-        THICKNESS = 5
-        CONTOUR_IDX = -1
-        BORDER = 10
 
         x, y, w, h = cv2.boundingRect(polys[0])
         # ROI (Region of interest)
         roi = img[y - BORDER: y + h + BORDER, x - BORDER: x + w + BORDER]
         roi_binary = binary[y - BORDER: y + h + BORDER, x - BORDER: x + w + BORDER]
         contours = img.copy()
-        cv2.drawContours(contours, polys, CONTOUR_IDX, RGB, THICKNESS)
+        cv2.drawContours(contours, polys, CONTOUR_IDX, RGB_WHITE, THICKNESS)
 
         imgs = []
 
@@ -59,18 +59,14 @@ def demonstration_test(path_tests):
         image = cv2.imread(str(p))
         img, binary, polys = get_fill_masks(image.copy())
         bbox = img.copy()
-        RGB = (255, 255, 255)
-        THICKNESS = 5
-        CONTOUR_IDX = -1
-        BORDER = 10
 
         contours = img.copy()
-        cv2.drawContours(contours, polys, CONTOUR_IDX, RGB, THICKNESS)
+        cv2.drawContours(contours, polys, CONTOUR_IDX, RGB_WHITE, THICKNESS)
         imgs_items = []
         for poly in polys:
             x, y, w, h = cv2.boundingRect(poly)
-            bbox = cv2.rectangle(bbox, (x, y), (x + w, y + h), (0, 0, 0), 5)
-            cv2.drawContours(img, polys, CONTOUR_IDX, RGB, THICKNESS)
+            bbox = cv2.rectangle(bbox, (x, y), (x + w, y + h), RGB_BLACK, THICKNESS)
+            cv2.drawContours(img, polys, CONTOUR_IDX, RGB_WHITE, THICKNESS)
             # ROI (Region of interest)
             roi = img[y - BORDER: y + h + BORDER, x - BORDER: x + w + BORDER]
             imgs_items.append(roi)
